f7d0805fd4fec596a3dfd1ff728e5293766b3bba
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2023-06-19 18:45:11 -0700

    push package

---
+++ reliablegpt/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='reliableGPT',
+    version='0.1.0',
+    description='Error handling and auto-retry library for GPT',
+    author='Ishaan Jaffer',
+    packages=['reliablegpt'],
+    install_requires=[
+        'openai',
+        'termcolor'
+    ],
+)
