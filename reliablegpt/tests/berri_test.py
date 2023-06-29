567474e955711911de185badbf2bde0c2b859828
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-06-28 17:35:42 -0700

    0.2.951

---
+++ reliablegpt/tests/berri_test.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import dotenv
+from dotenv import load_dotenv
+load_dotenv()
+
+sys.path.append('..')  # Adds the parent directory to the system path
+import openai
+from main import reliableGPT
+
+
+openai.ChatCompletion.create = reliableGPT(
+  openai.ChatCompletion.create, 
+  user_email= ["ishaan@berri.ai", "krrish@berri.ai"], 
+  user_token = "AxQgeB3aEDK2B3x4fNG3ZYJRvFfOfQuCTOR83Y_9y5g",
+  queue_requests=True,
+  fallback_strategy=['gpt-3.5-turbo', 'text-davinci-003', 'gpt-3.5-turbo'],
+  model_limits_dir = {"gpt-3.5-turbo": {"max_token_capacity": 40000, "max_request_capacity": 100}}
+)
+
+
+good_open_ai_api_key = os.getenv('OPENAI_API_KEY')
+
+model = "gpt-3.5-turbo"
+openai.api_key = good_open_ai_api_key
+messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant."
+    }, 
+    {
+        "role": "user",
+        "content": "Who is ishaan"
+    }
+]
+temperature = 0.7
+
+print("Making OpenAI Call")
+response = openai.ChatCompletion.create(model=model,
+                                            messages=messages,
+                                            temperature=temperature)
+
+print(response)
+
+
+
+
